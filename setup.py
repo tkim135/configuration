fb03f68beee833be5348bcec13e1ed439183e037
stv <stv@stanford.edu>
2016-03-01 20:53:11 +0000

    Add VPC
    Configurable VPC subnet
    
    Rename vpc
    
    Configure public sandbox
    
    VPC
    
    vpc
    
    subnet
    
    gateway
    
    More vpc cloudformation stuff
    
    Commit changes on disk
    
    Add vpc_sandbox role for Ansible
    
    Add new jenkins/sandbox plays
    
    Add sandbox "play-group"(?)
    
    Add gitignore
    
    boto stuff
    
    Remove network_acl stuff
    
    cleanup
    
    boom
    
    squash
    
    Ignore log file
    
    Cleanup
    
    Set default value for secure_dir
    
    Install Stanfords analytics_api
    
    Pin version of edx notes
    
    try fix cert file
    
    Ignore more files in git
    
    whitespace
    
    IMPORTANT: Add vars missing from upstream
    
    Only install templates if defined
    
    IMPORTANT: update file path for private key
    
    create ~/.ssh/config
    
    destroy/describe instances
    
    Cleanup
    
    wip
    
    Cleanup ssh/config for stage
    
    Remove unused approaches
    
    squash
    
    fix
    
    rename
    
    split out ec2
    
    move vpc
    
    wip
    
    squash
    
    wip
    
    wip
    
    wip
    
    squash
    
    squash
    
    squash
    
    squash
    
    wip
    
    docs
    
    docs
    
    docs
    
    docs
    
    docs
    
    docs
    
    wip
    
    wip
    
    squash
    
    squash
    
    squash
    
    squash
    
    squash
    
    squash
    
    squash
    
    superfluous args
    
    pep8
    
    squash
    
    squash
    
    squash
    
    docs
    
    squash
    
    wip
    
    clean
    
    squash
    
    comments
    
    docs
    
    squash
    
    squash
    
    squash
    
    squash
    
    squash
    
    imports
    
    reorder
    
    squash
    
    squash
    
    squash
    
    squash
    
    simplify
    
    wip
    
    squash
    
    squash
    
    squash
    
    move sandbox play
    
    symlink sandbox play
    
    wip
    
    wip
    
    squash
    
    more rules
    
    squash
    
    try fix ports
    
    funk
    
    squash
    
    elb
    
    squash
    
    squash
    
    cleanup vars
    
    unfork
    
    refactor
    
    squash
    
    fix
    
    edit play
    
    IMPORTANT try fix certs
    
    try fix
    
    add missing files
    
    unfork
    
    better host selection
    
    wip
    
    squash
    
    Revert "squash"
    
    This reverts commit 1ed68f3532322d4f0df2b6b0a78df9b89a91e9c0.
    
    fix trust issue
    
    fix template
    
    bool
    
    elb
    
    elb
    
    maybe fix template
    
    squash
    
    deploy all roles
    
    wip
    
    squash
    
    update play
    
    squash
    
    remove comments
    
    cleanup deploy helper
    
    script
    
    boom
    
    yup
    
    fix
    
    squash
    
    config comments
    
    programmatic ansible
    
    squash
    
    squash
    
    remove debug statements
    
    remove play
    
    config
    
    cleanup
    
    IMPORTANT KEEP THIS: remove export-ssh-config
    
    combine vpc role
    
    combine commands
    
    refactor
    
    cleanup
    
    split out tasks
    
    update cache path
    
    SQUASH ALL THE THINGS!
    
    wip
    
    Revert "SQUASH ALL THE THINGS!"
    
    This reverts commit 8f2d6c9d681922b2e845fdfacdba158f278712e9.
    
    Conflicts:
    	stanford/edxapp.yml
    
    Revert "wip"
    
    This reverts commit a120bc4be0ed266424158b6a5f43bdbc6def0912.
    
    Conflicts:
    	stanford/edxapp.yml
    
    s

---
+++ setup.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import find_packages
+from setuptools import setup
+
+
+_version = '1.0.0'
+with open('README.md') as file_input:
+    readme = file_input.read()
+
+
+setup(
+    # Metadata
+    name='openedx-configuration',
+    version=_version,
+    license='AGPL-3.0',
+    description='TODO: Short description',
+    long_description=readme,
+    author='stv',
+    author_email='stv@stanford.edu',
+    classifiers=[
+        'Development Status :: 6 - Mature',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+    ],
+    # URLs
+    url='https://github.com/Stanford-Online/configuration',
+    # Packages
+    # packages=find_packages('src'),
+    # include_package_data=True,
+    # package_dir={
+        # '': 'src',
+        # 'openedx_configuration': 'src/openedx_configuration',
+    # },
+    dependency_links=[
+        'https://github.com/edx/ansible/tarball/stable-1.9.3-rc1-edx#egg=ansible-1.9.3-edx',
+    ],
+    install_requires=[
+        'ansible==1.9.3-edx',
+        'awscli==1.4.2',
+        'boto==2.32.1',
+        'docopt==0.6.1',
+        'ecdsa==0.11',
+        'Jinja2==2.7.3',
+        'MarkupSafe==0.23',
+        'paramiko==1.15.1',
+        'prettytable==0.7.2',
+        'pycrypto==2.6.1',
+        'python-simple-hipchat==0.2',
+        'PyYAML==3.11',
+        'requests',
+        'wsgiref==0.1.2',
+    ],
+    tests_require=[
+    ],
+    cmdclass={
+        # 'test': Tox,
+    },
+    scripts=[
+        'bin/provision-sandbox',
+    ],
+)
