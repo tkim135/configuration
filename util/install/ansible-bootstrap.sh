e1fd0211d62282c2b7bd5a0c17ecb490da988fdc
stv <stv@stanford.edu>
2018-02-17 22:17:15 -0800

    Customize for stvstnfrd

--- util/install/ansible-bootstrap.sh
+++ util/install/ansible-bootstrap.sh
@@ -22,11 +22,11 @@ if [[ -z "${ANSIBLE_VERSION}" ]]; then
 fi
 
 if [[ -z "${CONFIGURATION_REPO}" ]]; then
-  CONFIGURATION_REPO="https://github.com/edx/configuration.git"
+  CONFIGURATION_REPO="https://github.com/stvstnfrd/configuration.git"
 fi
 
 if [[ -z "${CONFIGURATION_VERSION}" ]]; then
-  CONFIGURATION_VERSION="master"
+  CONFIGURATION_VERSION="ginkgo/wip"
 fi
 
 if [[ -z "${UPGRADE_OS}" ]]; then
